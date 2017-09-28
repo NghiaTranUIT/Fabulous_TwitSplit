004941f2c8fafe497fc81ee4165acd0e40ded43e
Nghia Tran <vinhnghiatran@gmail.com>
2017-09-28 11:14:11 +0700

    Create workspace with separate projects, Core and App

---
+++ TwitterCore/TwitterCore/TwitterCore.h
@@ -0,0 +1,19 @@
+//
+//  TwitterCore.h
+//  TwitterCore
+//
+//  Created by Nghia Tran on 9/28/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TwitterCore.
+FOUNDATION_EXPORT double TwitterCoreVersionNumber;
+
+//! Project version string for TwitterCore.
+FOUNDATION_EXPORT const unsigned char TwitterCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TwitterCore/PublicHeader.h>
+
+
